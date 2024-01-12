796f002ae59d49ecfe733ba06b848265db6d12ad
Venkat Choudhary Ala <venkatchoudhary.ala@gmail.com>
2024-01-12 17:41:08 +0530

    Update Page.py

--- LoginApp/Page.py
+++ LoginApp/Page.py
@@ -24,7 +24,8 @@ def main():
 def LoginPage():
 	ExceptFlag = 0
 	#st.title("<h1 style='text-align: center;'>_AGILE DASHBOARD_")
-	st.markdown("<h2 style='text-align: center; font-style: italic;'>AI BASED COURSE SYSTEM LOGIN</h2>", unsafe_allow_html=True)
+	st.markdown("<h2 style='text-align: center; font-style: italic;'>AI BASED COURSE SYSTEM</h2>", unsafe_allow_html=True)
+	st.markdown("<h4 style='text-align: center;'>LOGIN</h4>", unsafe_allow_html=True)
 	#st.write(" ")
 	st.subheader(" ")
 	st.subheader(" ")
@@ -62,6 +63,7 @@ def LoginPage():
 #@st.cache_data(experimental_allow_widgets=True)
 def SignUpPage():
 	st.markdown("<h2 style='text-align: center; font-style: italic;'>AI BASED COURSE SYSTEM SIGN UP</h2>", unsafe_allow_html=True)
+	st.markdown("<h4 style='text-align: center;'>SIGN UP</h4>", unsafe_allow_html=True)
 	#st.write(" ")
 	st.subheader(" ")
 	st.subheader(" ")

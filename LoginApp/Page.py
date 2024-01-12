ded04036c7396e996465d41bf80a09fa99756e74
Venkat Choudhary Ala <venkatchoudhary.ala@gmail.com>
2024-01-12 16:38:43 +0530

    Update Page.py

--- LoginApp/Page.py
+++ LoginApp/Page.py
@@ -84,7 +84,7 @@ def SignUpPage():
 					st.write("UserName Already Exists!! Try another..")
 
 			except FileNotFoundError:
-				Details = {"Name":UserName.strip(), "Password":HashPasswd(Passd.strip()), "Email": Email.strip(), "Role" : "UnVerified", "AccVerifStatus": "Un Verified", "Projects": [], "Tasks": []}
+				Details = {"Name":UserName.strip(), "Password":HashPasswd(Passd.strip()), "Email": Email.strip(), "SeenCount" : str(0), "AccVerifStatus": "Un Verified", "Answers": []}
 				UDetails = json.dumps(Details)
 				Path = os.path.join("UserAcc", UserName.strip() + ".ua")
 				with open(Path, "w") as File:

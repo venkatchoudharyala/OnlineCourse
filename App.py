4e3683fb877a097f66e6bef4d9085b11dae93a92
Venkat Choudhary Ala <venkatchoudhary.ala@gmail.com>
2024-02-04 09:52:48 +0530

    Update App.py

--- App.py
+++ App.py
@@ -138,8 +138,8 @@ def GeminiEvaluator(Question, Answer):
 	model = genai.GenerativeModel('gemini-pro')
 	prompt = "Hey Gemini this is Question: " + Question + " ; and this is the respective Answer: " + Answer + " ; Please evaluate the answer according to the question and just give the marks out of 10 and dont generate anything other than the marks..."
 	response = model.generate_content(prompt)
-	Marks = to_markdown(response.text)
-	st.write(Marks)
+	#Marks = to_markdown(response.text)
+	st.write(response.text)
 	return int(Marks)
 
 		
